1e08a640f2dee5c9a263f270f03e76c6d297eb10
Grace Chung <50254110+grchng@users.noreply.github.com>
2026-09-14 15:30:08 -0400

    rename room to env

--- utils/createRoomCode.ts
+++
@@ -1,8 +0,0 @@
-export function createRoomCode() {
-  const alphabet = "ABCDEFGHJKMNPQRSTUVWXYZ23456789";
-  let code = "";
-  for (let index = 0; index < 6; index++) {
-    code += alphabet[Math.floor(Math.random() * alphabet.length)];
-  }
-  return `${code.slice(0, 3)}-${code.slice(3)}`;
-}
